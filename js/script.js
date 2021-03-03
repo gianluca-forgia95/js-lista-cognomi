9a1d94e5329e8bed0a791e1cf2e3fd1bd853da84
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-03 14:39:01 +0100

    chiedo all'utente il cognome e creo lista

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+Chiedere all’utente il cognome
+inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+stampa la lista ordinata alfabeticamente
+scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+*/
+//chiedo all'utente un cognome
+var nuovoCognome = prompt('Inserisci il tuo cognome');
+//creo array cognomi
+var cognomi = ['Forgia', 'Rossi','Bianchi','Mancini'];
